4ed11d9ac9396dbca8703a74e0af01e6b3e9cce1
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-08-02 09:56:47 -0300

    refactor: make card into a component

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+
+  @Input({ required: true }) cardTitle!: string;
+
+}
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,16 +1,15 @@
 // material
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
 
 // angular and services
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
