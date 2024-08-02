f0e1eb460d25a0ce4ff9173816454b6e9ab1fe54
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-08-02 14:21:53 -0300

    feat: add snackbar to product create confirmation

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,19 +1,24 @@
+// Material
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+
+// angular
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
 import { RouterLink } from '@angular/router';
 import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [RouterLink, MatButtonModule, ReactiveFormsModule, MatInputModule],
+  imports: [RouterLink, MatButtonModule, ReactiveFormsModule, MatInputModule, MatSnackBarModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
   _productService = inject(ProductsService)
+  _snackBar = inject(MatSnackBar)
 
   form = new FormGroup({
     title: new FormControl<string>('', {nonNullable: true, validators: Validators.required})
@@ -22,7 +27,11 @@ export class CreateComponent {
   public onSubmit() {
     const payload = { title: this.form.controls.title.value };
     this._productService.create(payload).subscribe(() => {
-      alert("Sucesso")
+      this.openSnackBar("Produto criado com sucesso!", "fechar")
     });
+  }
+
+  private openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
   }
 }
