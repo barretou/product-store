cb91a8ffe80a1b9c7de71b9a010e594599581ea3
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-08-02 09:34:45 -0300

    feat: add product card

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,3 +1,8 @@
+// material
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+// angular and services
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
@@ -5,7 +10,7 @@ import { Product } from '../../shared/interfaces/product.interface';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -13,7 +18,7 @@ export class ListComponent implements OnInit{
   // Injects
   _productService = inject(ProductsService)
 
-  // Propriedades
+  // Properties
   products : Product[] = []
   
 
