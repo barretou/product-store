d0d26f4b29fdb9303e3ad84e8bfee6829aa058a3
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-08-02 00:37:34 -0300

    feat: add httpClient

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  products : any[] = []
+  _httpClient = inject(HttpClient)
 
+  ngOnInit(): void {
+    this._httpClient.get<any>("/api/products").subscribe((products)=> {
+      this.products = products
+    })
+  }
 }
