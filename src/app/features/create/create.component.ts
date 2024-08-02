eecce879e526a5c52e3b6e11b8ac850b28b0ce45
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-08-02 11:35:08 -0300

    refactor: lazy loading on createComponent

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
