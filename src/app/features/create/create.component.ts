0a67c3d2f5a7dd0471d3ec3122b6c87fcf497673
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-08-02 13:44:11 -0300

    feat: style form of product creation

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('', {nonNullable: true, validators: Validators.required})
+  });
 
+  public onSubmit() {
+    console.log(this.form.controls.title.value);
+  }
 }
