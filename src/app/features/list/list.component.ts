e2bad65aca6d40b21070a65b73ef3b62d2bff8ac
Gustavo Barreto <gustavo.barreto@aqtech.com>
2024-08-02 11:22:05 -0300

    feat: button create product added

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,15 +1,16 @@
 // material
-
+import {MatButtonModule} from '@angular/material/button';
 // angular and services
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
